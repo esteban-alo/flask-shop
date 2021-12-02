70058334d3871e71c2ef1206597b7143257ffcc4
Esteban Rodriguez Alonso <esteban@iMac-de-Esteban.local>
2021-12-02 10:19:01 -0500

    First commit

---
+++ app.py
@@ -0,0 +1,21 @@
+from flask import Flask
+from flask_jwt import JWT
+from flask_restful import Api
+
+from security import authenticate, identity
+from user import UserRegister
+from item import Item, ItemList
+
+app = Flask(__name__)
+app.config['PROPAGATE_EXCEPTIONS'] = True
+app.secret_key = '000-000-000-000'
+api = Api(app)
+
+jwt = JWT(app, authenticate, identity)
+
+api.add_resource(Item, '/items/<string:name>')
+api.add_resource(ItemList, '/items')
+api.add_resource(UserRegister, '/register')
+
+if __name__ == '__main__':
+    app.run(debug=True)  # important to mention debug=True
