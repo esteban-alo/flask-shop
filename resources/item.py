26307ba9172f81acb55baa545212f5620411cd24
Esteban Rodriguez Alonso <esteban@iMac-de-Esteban.local>
2021-12-02 19:38:56 -0500

    Updated ItemList resource

--- resources/item.py
+++ resources/item.py
@@ -61,14 +61,6 @@ class ItemList(Resource):
     TABLE_NAME = 'items'
 
     def get(self):
-        connection = sqlite3.connect('data.db')
-        cursor = connection.cursor()
-
-        query = "SELECT * FROM {table}".format(table=self.TABLE_NAME)
-        result = cursor.execute(query)
-        items = []
-        for row in result:
-            items.append({'name': row[0], 'price': row[1]})
-        connection.close()
-
+        # items = list(map(lambda item: item.json(), items))
+        items = [item.json() for item in ItemModel.get_all()]
         return {'items': items}
